301e472628afb321ab559453972c4168a1ec9a9d
w3cj <cj@null.computer>
2024-03-25 20:41:03 +0200

    amit

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Simple Node.js App', function() {
+    it('should return Hello, World! when accessing root URL', function() {
+        assert.equal('Hello, World!', 'Hello, World!');
+    });
+});
+
